2613f5f3588167bdffc10cb1b61b864f0d833320
Thomas Finnerty <34747130+tfinnerty02@users.noreply.github.com>
2019-12-17 12:16:27 -0500

    walkthrough pt. 3:

--- src/com/apcsa/model/Teacher.java
+++ src/com/apcsa/model/Teacher.java
@@ -1,13 +1,27 @@
 package com.apcsa.model;
 
 import java.sql.ResultSet;
-
 import com.apcsa.model.User;
+import java.sql.SQLException;
 
 public class Teacher extends User {
 	
+	/**
+	 * Creates an instance of the Teacher class.
+	 * 
+	 * @param user
+	 * @param rs
+	 * @throws SQLException
+	 */
+
 	public Teacher(User user, ResultSet rs) throws SQLException {
-		// TODO Auto-generated constructor stub
+	    super(user);
+	    
+	    this.teacherId = rs.getInt("teacher_id");
+	    this.departmentId = rs.getInt("department_id");
+	    this.firstName = rs.getString("first_name");
+	    this.lastName = rs.getString("last_name");
+	    this.departmentName = rs.getString("department_id");
 	}
 	
 	private int teacherId;
