2c726001a244d90e4e7aeed8cc74a6a036d106bd
Thomas Finnerty <34747130+tfinnerty02@users.noreply.github.com>
2019-12-17 12:14:46 -0500

    walkthrough pt. 2

--- src/com/apcsa/model/Administrator.java
+++ src/com/apcsa/model/Administrator.java
@@ -1,14 +1,26 @@
 package com.apcsa.model;
 
 import java.sql.ResultSet;
-
 import com.apcsa.model.User;
+import java.sql.SQLException;
 
 public class Administrator extends User {
 
-    public Administrator(User user, ResultSet rs) {
-		// TODO Auto-generated constructor stub
-
+	/**
+	 * Creates an instance of the Administrator class.
+	 * 
+	 * @param user
+	 * @param rs
+	 * @throws SQLException
+	 */
+	    
+	public Administrator(User user, ResultSet rs) throws SQLException {
+	    super(user);
+	    
+	    this.administratorId = rs.getInt("administrator_id");
+	    this.firstName = rs.getString("first_name");
+	    this.lastName = rs.getString("last_name");
+	    this.jobTitle = rs.getString("job_title");
 	}
     
 	private int administratorId;
